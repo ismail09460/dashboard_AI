fde2394f522f0d278c6cb6a12e24b6a888ed8731
hamzaKh11 <hamza.khalid@edu.uca.ma>
2022-05-29 21:09:57 +0100

    hamza commit 29/05/2022

--- myapp.py
+++ myapp.py
@@ -1,4 +1,5 @@
-# import libraries
+# # # # # # # # # # # # # # # # # # # # # === Import Libraries === # # # # # # # # # # # # # # # # # # # # # 
+
 from msilib.schema import tables
 import dash
 import dash_html_components as html
@@ -13,18 +14,20 @@ import dash_bootstrap_components as dbc
 from search_fct import search
 from dash import html
 
-# read data
+# # # # # # # # # # # # # # # # # # # # # === Read Data AS CSV === # # # # # # # # # # # # # # # # # # # # # 
+
 data = pd.read_csv("statistics_data.csv")
 
-# initialize dash
+# # # # # # # # # # # # # # # # # # # # # === Initialize Dash === # # # # # # # # # # # # # # # # # # # # # 
+
 app = dash.Dash()
 
-#initialize dash-bootstrap components
+# # # # # # # # # # # # # # # # # # # # # === Initialize Dash Bootstrap Components === # # # # # # # # # # # # # # # # # # # # # 
+
 app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 
+# # # # # # # # # # # # # # # # # # # # # === List OF Companies === # # # # # # # # # # # # # # # # # # # # # 
 
-
-# list of companies
 list_companies = []
 with open('companies.txt') as f:
     lines = f.readlines()
@@ -32,18 +35,34 @@ with open('companies.txt') as f:
         line = line.strip()
         list_companies.append(line)
 
-# test search
-# print(search('OCP'))
+# # # # # # # # # # # # # # # # # # # # # === Variable OF Each Company === # # # # # # # # # # # # # # # # # # # # # 
 
-#variables (example of OCP)
 Main_ORG = search('OCP')['Main_ORG']
 avril = search('OCP')['avril']
 mai = search('OCP')['mai']
 orgs = search('OCP')['orgs']
+texts = search('OCP')['texts']
 
+# # # # # # # # # # # # # # # # # # # # # === TABLE OF POST FOR EACH COMPANY === # # # # # # # # # # # # # # # # # # # # # 
 
+    # List of impresions
+impressions = []
+for text in list(dict.fromkeys(texts)):
+    impressions.append(data[data['text']==text]['sentiments'].tolist()[0])
+    
+    # Table of posts
+df = pd.DataFrame(
+    {
+        "id": np.arange(1, len(list(dict.fromkeys(texts)))+1),
+        "post": list(dict.fromkeys(texts)),
+        "impression": impressions
+    }
+)
 
-#component
+table = dbc.Table.from_dataframe(df, striped=True, bordered=True, hover=True)
+
+# # # # # # # # # # # # # # # # # # # # # === Components === # # # # # # # # # # # # # # # # # # # # # 
+
 app.layout = html.Div(children=[
     dbc.Row(
         html.Div([
@@ -55,110 +74,99 @@ app.layout = html.Div(children=[
         value = '--select company--')
         ]
         )
-    )
-,
-# add blank space between search bar and 2nd row to ensure visibility
-html.Div(
-  style={
-      "height":10  
-  }
-),
-#grid layout (2 columns)
-dbc.Row(
+    ),
+    # add blank space between search bar and 2nd row to ensure visibility
+    html.Div(
+    style={
+        "height":10  
+    }),
+    #grid layout (2 columns)
+    dbc.Row(
     [
         #first column contains informations and staistics
         dbc.Col([
             #this row contains 3 cards describing (company name ,posts number & impressions)
             dbc.Row(
                 [
-                    dbc.Col(  dbc.Card(
-            dbc.CardBody(
-                [
-                    html.H5("COMPANY NAME", className="card-title"),
-                    html.P(
-                        "This card has some text content.",
-                        className="card-text",
+                    dbc.Col(dbc.Card(
+                                dbc.CardBody(
+                                    [
+                                        html.H5("COMPANY NAME", className="card-title"),
+                                        html.P(
+                                            "This card has some text content.",
+                                            className="card-text",
+                                        ),
+                                    ]
+                                )
+                    )),
+                    dbc.Col(dbc.Card(
+                                dbc.CardBody(
+                                    [
+                                        html.H5("NUMBER OF POSTS", className="card-title"),
+                                        html.P(
+                                            "This card has some text content.",
+                                            className="card-text",
+                                        ),
+                                    ]
+                                )
+                            )
                     ),
-                ]
-            )
-        )),
-                    dbc.Col(
-                          dbc.Card(
-            dbc.CardBody(
-                [
-                    html.H5("NUMBER OF POSTS", className="card-title"),
-                    html.P(
-                        "This card has some text content.",
-                        className="card-text",
-                    ),
-                ]
-            )
-        )
-                    ),
-                    dbc.Col(  dbc.Card(
-            dbc.CardBody(
-                [
-                    html.H5("IMPRESSIONS", className="card-title"),
-                    html.P(
-                        "This card has some text content.",
-                        className="card-text",
-                    ),
-                ]
-            )
-        ))
+                    dbc.Col(dbc.Card(
+                                dbc.CardBody(
+                                    [
+                                        html.H5("IMPRESSIONS", className="card-title"),
+                                        html.P(
+                                            "This card has some text content.",
+                                            className="card-text",
+                                        ),
+                                    ]
+                                )
+                            ))
                 ]
             ),
-            dbc.Row()
+            # add blank space between search bar and 2nd row to ensure visibility
+            html.Div(
+            style={
+                "height":10  
+            }),
+            # table of posts
+            dbc.Row(
+               html.Div([table])
+            )
         ]),
         #second column(consists of 2 rows) contains 2 plots 
         dbc.Col([
             #first row contains a line chart (number of posts per month)
             dbc.Row(
-               html.Div([
-            dcc.Graph(id = 'bar_plot')]
-        )
+               html.Div([dcc.Graph(id = 'bar_plot')])
             ),
              #second row contains a bubble chart (number of posts per month)
-            dbc.Row(
-                html.Div(["bubble chart"])
-            )
+            dbc.Row(html.Div(["bubble chart"]))
             ]
         )
     ],
     className="g-0",
-)
-    ])
+    )
+])
 
 
+# # # # # # # # # # # # # # # # # # # # # === CAll BACK === # # # # # # # # # # # # # # # # # # # # # 
 
+# @app.callback(Output(component_id='bar_plot', component_property= 'figure'),
+#               [Input(component_id='dropdown', component_property= 'value')])
+# def graph_update(dropdown_value):
+#     fig = go.Figure([go.Scatter(x = data['month'], y = avril['number_of_posts'],\
+#                      line = dict(color = 'firebrick', width = 4))
+#                      ])
     
-@app.callback(Output(component_id='bar_plot', component_property= 'figure'),
-              [Input(component_id='dropdown', component_property= 'value')])
-def graph_update(dropdown_value):
-    fig = go.Figure([go.Scatter(x = data['month'], y = avril['number_of_posts'],\
-                     line = dict(color = 'firebrick', width = 4))
-                     ])
+#     fig.update_layout(title = 'Number Of Post In Each Month',
+#                       xaxis_title = 'Dates',
+#                       yaxis_title = 'Number OF Post'
+#                       )
+#     return fig
     
-    fig.update_layout(title = 'Number Of Post In Each Month',
-                      xaxis_title = 'Dates',
-                      yaxis_title = 'Number OF Post'
-                      )
-    return fig
-    
-# fig = px.bar(data, x="month", y="MAIN_ORG", color="sentiments", barmode="group")
 
-# app.layout = html.Div(children=[
-#     html.H1(children='Hello Dash'),
-
-#     html.Div(children='''
-#         Dash: A web application framework for your data.
-#     '''),
-
-#     dcc.Graph(
-#         id='example-graph',
-#         figure=fig
-#     )
-# ])
+# # # # # # # # # # # # # # # # # # # # # === MAIN === # # # # # # # # # # # # # # # # # # # # # 
 
 if __name__ == '__main__': 
     app.run_server()
